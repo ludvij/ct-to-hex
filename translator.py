383c2b1b40fba76bd16b8fb772d04206a5194abe
ludvij <Luisvijande@gmail.com>
2020-04-28 18:52:26 +0200

    lotta things

--- translator.py
+++ translator.py
@@ -1,26 +1,29 @@
 from dotenv import load_dotenv
 from os import getenv
+from sys import argv
 import re
 
-load_dotenv(r"C:\Users\luisv\Desktop\translator\instructions.env")
+regex = []
+regex_path = r".\dependancies\re"
+env_path = r".\dependancies\instructions.env"
+
+hex_symbols=["a","b","c","d","e","f","A","B","C","D","E","F","h"]
 
 # gets the regex for all expressions
-def get_reg():
-    reg = []
-    with open("re.txt", "r") as f:
-        for l in f.readlines():
-            l = l.replace("\n","")
-            reg.append(l)
-    return reg
+def load_regex():
+    global regex
+    with open(regex_path, "r") as f:
+        regex = f.read().split('\n')
 
 def check_input(r, l):
     check = re.match(r, l)
     return check
 
 
+
 # gets the title of the instruction in instructions.env
 def get_instruction(check):
-    print(check.group(0))
+    print(check.group(0), end=" --> ")
     group1 = check.group(1)
 
     if group1 == "mov":
@@ -44,70 +47,146 @@ def get_instruction(check):
         return str(group1)
 
 # transforms registers to their bin value
-def get_register_bin(check, groupN):
-    g = check.group(groupN)
-    g = g.replace("[","").replace("]","")
-    g = bin(int(g[1]))[2:]
-    return f"{g:0>3}"
+def get_register_bin(check, element):
+    register = check.group(element)
+    register = int(register.replace("[","").replace("]","")[1:])
+
+    if 0 > register or register > 7 :
+        handle_error(f"the register [r{register}] is not valid")
+
+    register = bin(register)[2:]
+
+    return f"{register:0>3}"
 
 # transfomrs hex values to bin for convenience
-def get_inm8_bin(check, groupN):
-    g = bin(int(check.group(groupN), 16))[2:]
-    g = f"{g:0>8}"
-    return g
+def get_inm8_bin(check, element):
+    inm8 = check.group(element)
+
+    if any(ele in inm8 for ele in hex_symbols):
+        inm8 = inm8.replace('h','')
+
+        if len(str(inm8)) > 2:
+            handle_error(f"{inm8} is not a 8 bit number")
+
+        inm8 = bin(int(inm8, 16))[2:]
+        
+    else:
+        inm8 = int(inm8)
+
+        if 0 > inm8 or 255 < inm8:
+            handle_error(f"{inm8} is not a 8 bit number")
+
+        inm8 = bin(inm8)[2:]
+
+    return f"{inm8:0>8}"
 
 
 # gets the instruction template from instructions.env
 # and transfomrs them to binary
 def set_bin(instruction, check):
-    i_bin = getenv(instruction)
-    i_bin = i_bin.split('_')
+    instruction_bin = getenv(instruction)
+    instruction_bin = instruction_bin.split('_')
     
-    if len(i_bin) == 1:
+    if len(instruction_bin) == 1:
         pass
 
-    elif len(i_bin) == 2:
-        i_bin[1] = get_inm8_bin(check, 2)
+    elif len(instruction_bin) == 2:
+        instruction_bin[1] = get_inm8_bin(check, 2)
 
-    elif len(i_bin) == 3:
-        i_bin[1] = get_register_bin(check, 2)
-        if ("inm8" in i_bin[2]):
-            i_bin[2] = get_inm8_bin(check, 3)
+    elif len(instruction_bin) == 3:
+        instruction_bin[1] = get_register_bin(check, 2)
+        if ("inm8" in instruction_bin[2]):
+            instruction_bin[2] = get_inm8_bin(check, 3)
 
-    elif len(i_bin) == 4:
-        i_bin[1] = get_register_bin(check, 2)
-        i_bin[2] = get_register_bin(check, 3)
+    elif len(instruction_bin) == 4:
+        instruction_bin[1] = get_register_bin(check, 2)
+        instruction_bin[2] = get_register_bin(check, 3)
 
-    elif len(i_bin) == 5:
-        i_bin[1] = get_register_bin(check, 2)
-        i_bin[2] = get_register_bin(check, 3)
-        i_bin[3] = get_register_bin(check, 4)
+    elif len(instruction_bin) == 5:
+        instruction_bin[1] = get_register_bin(check, 2)
+        instruction_bin[2] = get_register_bin(check, 3)
+        instruction_bin[3] = get_register_bin(check, 4)
 
-    return "".join(i_bin)
+    return "".join(instruction_bin)
 
 
 # transfomr the binary instructions to hexadecimal
-def set_hex(i_bin):
-    i_hex = hex(int(i_bin, 2))[2:]
+def set_hex(instruction_bin):
+    instruction_hex = hex(int(instruction_bin, 2))[2:]
 
-    return f"{i_hex:0>4}"
+    return f"{instruction_hex:0>4}"
+
+def set_to_ct(check):
+    instruction = get_instruction(check)
+    instruction_bin = set_bin(instruction, check)
+    res = set_hex(instruction_bin).upper()
+
+    print(res + "h")
+
+    return res
+
+def handle_error(msg):
+    print(msg)
+
+    exit()
+
+def operate(expression, text = []):
+    is_valid = False
+
+    for r in regex:
+        check = check_input(r, expression)
+
+        if check != None:
+            ct_expression = set_to_ct(check)
+            text.append(ct_expression)
+            is_valid = True
+
+    return is_valid
+
+def operate_file():
+    text = []
+    with open("input.txt", "r") as f:
+        for counter, line in enumerate(f.readlines()):
+
+            line = line.replace('\n', '')
+            is_valid = operate(line, text)
+
+            if (not is_valid):
+                handle_error(f"LEARN HOW TO WRITE GODDAMMIT!!!!\nThe instruction [{line}] introduced at line [{counter}] is not a valid one.")
+
+    with open("output.mem", "w") as f:
+        f.write("\n".join(text))
+
+def operate_input():
+    expression = input("input preffered expression, exit to close\n")
+
+    while (expression != "exit"):
+        operate(expression)
+        expression = input()
+
+def operate_command_line():
+    expression = " ".join(argv[2:])
+
+    is_valid = operate(expression)
+    
+    if (not is_valid):
+        print("you wot mate?")
+
 
 def main():
-    reg = get_reg()
 
-    with open("output.mem", "w") as o:
-        with open("input.txt", "r") as i:
-            for l in i.readlines():
-                l = l.replace("\n", "")
-                for r in reg:
-                    check = check_input(r, l)
-                    if check != None:
-                        instruction = get_instruction(check)
-                        i_bin = set_bin(instruction, check)
-                        i_hex = set_hex(i_bin)
-                        o.write(f"{i_hex.upper()}\n")
+    if len(argv) == 1:
+        operate_file()
+    elif argv[1] == "-i":
+        operate_input()
+    elif argv[1] == "-c":
+        operate_command_line()
+    else:
+        print("invalid command")
 
 
 
 if __name__ == "__main__":
+    load_dotenv(env_path)
+    load_regex()
     main()
